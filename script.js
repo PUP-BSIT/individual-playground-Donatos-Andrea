c350f244ac1d571706cd06c4ecff6ebe2f72f1bf
Andreadonatos <dreadonatos@gmail.com>
2024-09-23 19:38:21 +0800

    add changes

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 var Animal;
 (function (Animal) {
-    Animal["CAPYBARA"] = "Capybara";
-    Animal["CAT"] = "Cat";
-    Animal["DOG"] = "Dog";
-    Animal["HORSE"] = "Horse";
-    Animal["CHICKEN"] = "Chicken";
+    Animal["MAMMAL"] = "mammals";
+    Animal["REPTILE"] = "reptile";
+    Animal["AMPHIBIAN"] = "ambphibian";
+    Animal["BIRD"] = "bird";
+    Animal["FISH"] = "fish";
 })(Animal || (Animal = {}));
-/*type Profile = {
-    name: string;
-    role: string;
-    age: number;
-    gender: string;
-    birthday: Date;
-
-    print(): void;
-}
-
-const mikaProfile: Profile = {
-    name: CitrusMember.MIKAELLA,
-    role: "Techlead",
-    age: 21,
-    gender: "Female",
-    birthday: new Date(2003, 5, 15),
-
-    print() {
-        console.log(`Profile Details:
-        Name: ${this.name}
-        Role: ${this.role}
-        Age: ${this.age}
-        Gender: ${this.gender}
-        Birthday: ${this.birthday.toDateString()}`);
+var cat = {
+    animal: Animal.MAMMAL,
+    color: "white",
+    lifespan: "10 years",
+    gender: "female",
+    print: function () {
+        console.log("I want a cat, that is color ".concat(this.color, ", \n            with a lifespan of ").concat(this.lifespan, ", and a ").concat(this.gender, ". \n            Cat's are part of a ").concat(this.animal, " group and they can \n            help us with stress that boost our mental health."));
     }
-}
-
-mikaProfile.print(); */ 
+};
+var snowyOwl = {
+    animal: Animal.BIRD,
+    color: "white",
+    lifespan: "10-12 years",
+    gender: "male",
+    print: function () {
+        console.log("In my whole life, I have never had a pet that is a ".concat(this.animal, ".  +\n          If I could have one, I would like to have a snowy owl  +\n          because it has a color of ").concat(this.color, " and is a ").concat(this.gender, ".  +\n          Even when it's old, it would still be white.  +\n          Snowy owls have an incredible lifespan of up to ").concat(this.lifespan, ",  +\n          which is amazing. I can grow old with them."));
+    },
+};
+var axolotl = {
+    animal: Animal.AMPHIBIAN,
+    color: "white",
+    lifespan: "6 years",
+    gender: "female",
+    print: function () {
+        console.log("I have a".concat(this.gender, " ").concat(this.color, " Axolotl, \n            with a lifespan of ").concat(this.lifespan, ".\n            She is classified as ").concat(this.animal, "."));
+    }
+};
+cat.print();
+snowyOwl.print();
+axolotl.print();
--- script.ts
+++ script.ts
@@ -1,36 +1,64 @@
 enum Animal {
-    CAPYBARA = "Capybara",
-    CAT = "Cat",
-    DOG = "Dog",
-    HORSE = "Horse",
-    CHICKEN = "Chicken"
+    MAMMAL = "mammals",
+    REPTILE = "reptile",
+    AMPHIBIAN = "ambphibian",
+    BIRD = "bird",
+    FISH = "fish"
 }
 
-/*type Profile = {
-    name: string;
-    role: string;
-    age: number;
-    gender: string;
-    birthday: Date;
-
+type Pets = {
+    animal: Animal;
+    color: string;
+    lifespan: string;
+    gender: string; 
     print(): void;
 }
 
-const mikaProfile: Profile = {
-    name: CitrusMember.MIKAELLA,
-    role: "Techlead",
-    age: 21,
-    gender: "Female",
-    birthday: new Date(2003, 5, 15), 
-
-    print() {
-        console.log(`Profile Details:
-        Name: ${this.name}
-        Role: ${this.role}
-        Age: ${this.age}
-        Gender: ${this.gender}
-        Birthday: ${this.birthday.toDateString()}`);
+const cat: Pets = {
+    animal: Animal.MAMMAL,
+    color: "white",
+    lifespan: "10 years",
+    gender: "female",
+    print(){
+        console.log (
+            `I want a cat, that is color ${this.color}, 
+            with a lifespan of ${this.lifespan}, and a ${this.gender}. 
+            Cat's are part of a ${this.animal} group and they can 
+            help us with stress that boost our mental health.`);
     }
-}
+};
 
-mikaProfile.print(); */
+const snowyOwl: Pets = {
+    animal: Animal.BIRD,
+    color: "white",
+    lifespan: "10-12 years",
+    gender: "male",
+    print() {
+      console.log(
+        `In my whole life, I have never had a pet that is a ${this.animal}.  +
+          If I could have one, I would like to have a snowy owl  +
+          because it has a color of ${this.color} and is a ${this.gender}.  +
+          Even when it's old, it would still be white.  +
+          Snowy owls have an incredible lifespan of up to ${this.lifespan},  +
+          which is amazing. I can grow old with them.`
+      );
+    },
+};
+  
+
+const axolotl: Pets = {
+    animal: Animal.AMPHIBIAN,
+    color: "white",
+    lifespan: "6 years",
+    gender: "female",
+    print(){
+        console.log (
+            `I have a${this.gender} ${this.color} Axolotl, 
+            with a lifespan of ${this.lifespan}.
+            She is classified as ${this.animal}.`);
+    }
+};
+
+cat.print();
+snowyOwl.print();
+axolotl.print();
